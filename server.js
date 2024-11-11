3ac1ea746bc0cda86055604dc96ce3aa1a7a1822
jomi087 <jomi>
2024-11-11 17:52:15 +0530

    serverjs

--- server.js
+++ server.js
@@ -20,13 +20,11 @@ app.use('/',userRoute)
 //for admin_routes     //recomended to give separate routed
 const adminRoute = require('./routes/adminRoute')
 app.use('/admin',adminRoute)
-
  
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}/`);
     console.log(`http://43.204.245.198:${PORT}/`);
     console.log(`https://jomi.shop/`);
-    console.log(`serverrunning`);
 })
 
 
